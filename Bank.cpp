f40e12cee5447ca2a292f0db1f5eb2ea9d473695
Unknown_Coder <akshaykumar954012@gmail.com>
2025-12-14 19:12:13 +0530

    Refactor BankAccount class and menu functionality
    Refactored BankAccount class and menu function to improve functionality and structure. Added static count and enhanced user interaction.

--- Bank.cpp
+++ Bank.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 using namespace std;
 
-/*class BankAccount{
+class BankAccount{
     int accountNumber;
     int balance;
     int transactions[5];
     int amount;
+    static int count;
     public:
         void openAccount(); // input accountNumber and initial Balance
         void deposits(int amount); // add amount to balance and store in array
@@ -13,28 +14,65 @@ using namespace std;
         void showBalance(); // display current balance
         void showTransactions(); // display all transactions
 };
-*/
 
-/*void BankAccount :: openAccount(){
+
+void BankAccount :: openAccount(){
+    account[count];
+    cout << "Enter Your Account Number: ";
+    cin>>accountNumber;
+    cout << "Enter Your Balance: ";
+    cin >> balance;
 }
-void BankAccount :: deposits(int amount){
+/*void BankAccount :: deposits(int amount){
 }
 void BankAccount :: withdraw(int amount){
-} 
+}
 void BankAccount :: showBalance(){
+    
+    cout << "Current Balance: " <<balance<< endl;
 }
 void showTransactions(){
 }
 */
 
 void menu(void){
-    int choice;
-    cout<<"1 --> OpenAccount\n2 --> Deposits\n3 --> Withdraw\n4 --> showBalance\n5 --> showTransactions\n"<<endl;
-    cout<<"Enter Your Choice: ";
-    cin>>choice;
-    /*switch(choice){
-        case 1:
-        acc.openAccount();
+    while(1){
+        int choice;
+        cout<<"1 --> OpenAccount\n2 --> Deposits\n3 --> Withdraw\n4 --> showBalance\n5 --> showTransactions\n6 --> Exit"<<endl;
+        cout<<"Enter Your Choice: ";
+        cin>>choice;
+        switch(choice){
+            case 1:
+            acc.openAccount();
+            break;
+            case 2:
+            acc.deposits();
+            break;
+            case 3:
+            acc.withdraw();
+            break;
+            case 4:
+            acc.showBalance();
+            break;
+            case 5:
+            acc.showTransactions();
+            break;
+            case 6;
+            goto end;
+            break;
+        }
+        menu();
+    }
+    end:
+}
+
+int main(){
+    BankAccount account[20];
+    cout<<"\t\t***Welcome_To_Our_Money_bank***\n"<<endl;
+    menu();
+    //BankAccount acc;
+    return 0;
+}        acc.openAccount();
         break;
         case 2:
         acc.deposits();
